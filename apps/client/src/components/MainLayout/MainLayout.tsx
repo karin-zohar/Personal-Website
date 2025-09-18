77b5662afe48eef6e3fd92861fb002a973ee39bb
Karin Zohar <karinzohar3@gmail.com>
2025-09-18 21:06:31 +0300

    feat: create main layout

---
+++ apps/client/src/components/MainLayout/MainLayout.tsx
@@ -0,0 +1,24 @@
+import React, { FC, ReactNode } from 'react';
+import useStore from '@/store/store';
+import clsx from 'clsx';
+import './main-layout.style.css'
+
+type MainLayoutProps = {
+    children: ReactNode;
+};
+
+const MainLayout: FC<MainLayoutProps> = ({ children }) => {
+    const { isLightTheme } = useStore()
+    return (
+        <div className={clsx('main-layout', 'theme', isLightTheme ? 'light' : 'dark')}>
+
+            <header>header</header>
+            <main>
+                {children}
+            </main>
+        </div>
+
+    );
+};
+
+export default MainLayout;
