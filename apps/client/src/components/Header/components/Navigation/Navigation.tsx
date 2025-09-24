d2d9d2ff961282f32a79649271d274e7aa40e97b
Karin Zohar <karinzohar3@gmail.com>
2025-09-24 20:01:01 +0300

    feat: style nav drawer layout

--- apps/client/src/components/Header/components/Navigation/Navigation.tsx
+++ apps/client/src/components/Header/components/Navigation/Navigation.tsx
@@ -27,7 +27,7 @@ const Navigation: FC<NavigationProps> = ({ heroRef }) => {
       ([hero]) => {
         setIsTopNav(hero.isIntersecting);
       },
-      { threshold: 0 }
+      { threshold: 0.2 }
     );
 
     observer.observe(heroRef.current);
