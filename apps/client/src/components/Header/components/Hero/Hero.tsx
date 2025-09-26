23beb251cf5665c244e6ddf3a4d361e77543281e
Karin Zohar <karinzohar3@gmail.com>
2025-09-26 17:34:45 +0300

    feat: add gradient to hero background

--- apps/client/src/components/Header/components/Hero/Hero.tsx
+++ apps/client/src/components/Header/components/Hero/Hero.tsx
@@ -2,7 +2,14 @@ import React, { forwardRef } from "react";
 
 const Hero = forwardRef<HTMLDivElement>((_props, ref) => {
   return (
-    <div ref={ref} style={{ backgroundColor: "var(--teal-500)", height: 500 }}>
+    <div
+      ref={ref}
+      style={{
+        background:
+          "linear-gradient(160deg, var(--accent) 10%, transparent 50%",
+        height: 500,
+      }}
+    >
       {/* Hero content */}
     </div>
   );
