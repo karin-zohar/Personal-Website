410364ad9415a131bca93429bc289b93837bdf05
Karin Zohar <karinzohar3@gmail.com>
2025-09-27 09:52:57 +0300

    feat: add custom icons to the open/close drawer buttons

--- apps/client/libs/ui/icons/index.tsx
+++ apps/client/libs/ui/icons/index.tsx
@@ -2,3 +2,5 @@ export { FiSun as SunIcon } from "react-icons/fi";
 export { FiMoon as MoonIcon } from "react-icons/fi";
 export { FaAngleDown as ArrowDownIcon } from "react-icons/fa6";
 export { CiGlobe as GlobeIcon } from "react-icons/ci";
+export { CgMenuRightAlt as MenuIcon } from "react-icons/cg";
+export { IoCloseOutline as CloseIcon } from "react-icons/io5";
