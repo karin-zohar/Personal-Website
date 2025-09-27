a3b8f0da3e201c1155a0a58f8b3d9da7cc04eb31
Karin Zohar <karinzohar3@gmail.com>
2025-09-27 14:48:50 +0300

    feat: create initial responsive hero layout

--- apps/client/src/components/Header/components/Hero/Hero.tsx
+++ apps/client/src/components/Header/components/Hero/Hero.tsx
@@ -1,10 +1,39 @@
 import React, { forwardRef } from "react";
 import "./hero.style.css";
+import { Flex, Typography } from "antd";
+import useStore from "@/store/store";
 
 const Hero = forwardRef<HTMLDivElement>((_props, ref) => {
+  const { getLocalizedText } = useStore();
+  const { Title, Text } = Typography;
+
+  const heroText = {
+    title: {
+      english: "Karin Zohar",
+      hebrew: "קארין זוהר",
+    },
+    bio: {
+      english:
+        "I'm a curious, creative and detail oriented Front-End Developer with a passion for creating amazing user experiences with clean and maintainable code.",
+      hebrew:
+        "אני מפתחת פרונטאנד סקרנית ויצירתית עם תשומת לב לפרטים ותשוקה ליצירת חוויות משתמש מדהימות עם קוד נקי וקל לתחזוקה.",
+    },
+  };
   return (
     <div className={"hero"} ref={ref}>
-      {/* Hero content */}
+      <Flex className="hero-content">
+        <div className="hero-img-container">
+          <img
+            src="https://images.unsplash.com/photo-1758344953670-c15779f89ed4?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <Flex className="hero-content-text" vertical>
+          <Title level={1}>{getLocalizedText(heroText.title)}</Title>
+          <Text className="bio">{getLocalizedText(heroText.bio)}</Text>
+          <div className="links">XXX</div>
+        </Flex>
+      </Flex>
     </div>
   );
 });
