e359ac4ac5df533e46793779f417d46d72ded403
Karin Zohar <karinzohar3@gmail.com>
2025-09-29 23:35:21 +0300

    feat: add temporary hero image

--- apps/client/src/components/Header/components/Hero/Hero.tsx
+++ apps/client/src/components/Header/components/Hero/Hero.tsx
@@ -3,13 +3,11 @@ import useStore from "@/store/store";
 import { Flex, Typography } from "antd";
 import SocialLinks from "@/components/SocialLinks/SocialLinks";
 import "./hero.style.css";
+import { default as HeroImage } from "@/assets/img/hero-karin.jpg";
 
 const Hero = () => {
   const { getLocalizedText } = useStore();
   const { Title, Text } = Typography;
-
-  const heroImage =
-    "https://plus.unsplash.com/premium_photo-1678566154673-a728037f3f00?q=80&w=702&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
   const heroText = {
     title: {
@@ -28,7 +26,7 @@ const Hero = () => {
     <div className={"hero"}>
       <Flex className="hero-content">
         <div className="hero-img-container">
-          <img src={heroImage} alt="" />
+          <img src={HeroImage} alt="" />
         </div>
         <Flex className="hero-content-text" vertical>
           <Title level={1}>{getLocalizedText(heroText.title)}</Title>
