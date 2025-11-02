f2d72fcae5cd532931140ed353a740bab252aa4c
Karin Zohar <karinzohar3@gmail.com>
2025-11-02 18:41:46 +0200

    fix: authorize preview to use the contact endpoint

--- apps/server/api/contact.ts
+++ apps/server/api/contact.ts
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
   );
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.header("Access-Control-Allow-Origin", "POST, OPTIONS");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
