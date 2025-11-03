4baa781a68c164f10e831940dc81b2d047607801
Karin Zohar <karinzohar3@gmail.com>
2025-11-03 14:38:19 +0200

    feat: remove email options list and scroll to contact section instead

--- apps/client/src/components/SocialLinks/SocialLinks.tsx
+++ apps/client/src/components/SocialLinks/SocialLinks.tsx
@@ -1,15 +1,8 @@
 import React from "react";
 
-import {
-  ArrowOutwardIcon,
-  CheckmarkIcon,
-  EmailIcon,
-  GithubIcon,
-  LinkedinIcon,
-} from "@/libs/ui/icons";
-import { Button, Flex, List, Popover } from "antd";
+import { EmailIcon, GithubIcon, LinkedinIcon } from "@/libs/ui/icons";
+import { Button, Flex } from "antd";
 import useStore from "@/store/store";
-import useResettableCopyToClipboard from "@/libs/ui/hooks/useResettableCopyToClipboard";
 import "./socialLinks.style.css";
 
 const socialLinks = [
@@ -25,65 +18,8 @@ const socialLinks = [
   },
 ];
 
-const email = "karinzohar3@gmail.com";
-const subject = "Cool website! Can I hire you?";
-const body = "Let's chat!";
-
 const SocialLinks = () => {
-  const [copyState, copyToClipboard, resetCopyState] =
-    useResettableCopyToClipboard();
-  const { getLocalizedText } = useStore();
-
-  const emailOptions = [
-    {
-      text: "gmail",
-      link: `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(
-        email
-      )}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`,
-    },
-    {
-      text: "outlook",
-      link: `https://outlook.office.com/mail/deeplink/compose?to=${encodeURIComponent(
-        email
-      )}&subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(
-        body
-      )}`,
-    },
-    {
-      text: getLocalizedText({
-        english: "Copy email address",
-        hebrew: "העתקת כתובת מייל",
-      }),
-      onClick: () => copyToClipboard(email),
-    },
-  ];
-
-  const emailOptionsList = (
-    <List
-      className="email-options-list"
-      dataSource={emailOptions}
-      renderItem={({ link, text, onClick }) => (
-        <List.Item>
-          {link && (
-            <a href={link} target="_blank">
-              <span>{text} </span>
-              <ArrowOutwardIcon />
-            </a>
-          )}
-          {onClick && (
-            <Button
-              type="text"
-              onClick={onClick}
-              icon={!copyState.error && copyState.value && <CheckmarkIcon />}
-              iconPosition="end"
-            >
-              {text}
-            </Button>
-          )}
-        </List.Item>
-      )}
-    />
-  );
+  const { scrollTo } = useStore();
 
   return (
     <Flex className="social-links">
@@ -99,16 +35,13 @@ const SocialLinks = () => {
         </a>
       ))}
 
-      <Popover
-        content={emailOptionsList}
-        placement="bottom"
-        arrow={false}
-        onOpenChange={resetCopyState}
-      >
-        <Flex className="link-icon-wrapper">
-          <EmailIcon />
-        </Flex>
-      </Popover>
+      <Button
+        key={"email"}
+        className="link-icon-wrapper email-icon-wrapper"
+        type="text"
+        onClick={() => scrollTo("contact")}
+        icon={<EmailIcon />}
+      ></Button>
     </Flex>
   );
 };
