09fcce3417123fd59b336510c3519280c5fcfe05
Karin Zohar <karinzohar3@gmail.com>
2025-11-25 14:18:53 +0200

    feat: style chat input

--- apps/client/src/components/Main/components/Chatbot/components/ChatInput.tsx
+++ apps/client/src/components/Main/components/Chatbot/components/ChatInput.tsx
@@ -1,6 +1,6 @@
 import GenFormItem from "@/libs/ui/components/GenFormItem/GenFormItem";
 import { ArrowUpIcon } from "@/libs/ui/icons";
-import { Button, Flex, Form, Input } from "antd";
+import { Button, Form, Input } from "antd";
 import React, { FC } from "react";
 
 type ChatInputProps = {
@@ -9,13 +9,11 @@ type ChatInputProps = {
 
 const ChatInput: FC<ChatInputProps> = ({ placeholder }) => {
   return (
-    <Form>
-      <Flex>
-        <GenFormItem>
-          <Input.TextArea placeholder={placeholder} rows={1} />
-        </GenFormItem>
-        <Button icon={<ArrowUpIcon />} />
-      </Flex>
+    <Form className="chat-input-form">
+      <GenFormItem className="chat-input">
+        <Input.TextArea placeholder={placeholder} rows={1} />
+      </GenFormItem>
+      <Button className="send-prompt-button" icon={<ArrowUpIcon />} />
     </Form>
   );
 };
