d1b2c61e65c5892c7b3f6c85f43e905b65dc32bc
Karin Zohar <karinzohar3@gmail.com>
2026-01-21 19:59:32 +0200

    build: include static files in build

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,15 +1,32 @@
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+
+    viteStaticCopy({
+      targets: [
+        {
+          src: "apps/server/src/data/rag/**/*",
+          dest: "data/rag",
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@/libs/": `${path.resolve(__dirname, "libs")}/`,
       "@/": `${path.resolve(__dirname, "src")}/`,
     },
+  },
+  build: {
+    outDir: "dist",
+    ssr: true,
   },
   server: {
     port: 3000,
--- apps/server/src/data/rag/index.ts
+++ apps/server/src/data/rag/index.ts
@@ -6,6 +6,3 @@ const __dirname = path.dirname(__filename);
 
 // This exports the absolute path to this specific folder
 export const RAG_DATA_PATH = __dirname;
-
-export const profile = path.resolve("./", "karin-profile.md");
-export const cv = path.resolve("./", "Karin Zohar - CV.pdf");
